659dd2e5d8ce1f3565c5cee713a7c12397e91102
Vincent Bensch <vbensch@gmail.com>
2014-06-08 08:57:46 -0500

    Before Vacation

--- Antonymous.c
+++ Antonymous.c
@@ -1,7 +1,17 @@
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  none)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Motor,  mtr_S1_C2_1,     Drive_R,       tmotorTetrix, PIDControl, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C2_2,     Drive_L,       tmotorTetrix, PIDControl, encoder)
+
+#include "Header.h"
 
 task main()
 {
+	waitForStart();
 
-
-
+	drive(38,true);
+	turn(-55);
+	drive(16,true);
+	turn(95);
+	drive(13,true);
 }
--- Teleop.c
+++ Teleop.c
@@ -1,7 +1,29 @@
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  none)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Motor,  mtr_S1_C2_1,     Drive_R,       tmotorTetrix, PIDControl, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C2_2,     Drive_L,       tmotorTetrix, PIDControl, encoder)
+
+#include "Header.h"
 
 task main()
 {
+	waitForStart();
+	while(true)
+	{
+		grabjoys();
 
+		if(resetbtn){reset();}
 
+		if(overidebtn){overide();}
 
+		if(killbtn){kill();}
+
+		if(boostbtn){boost();}
+
+		if(turnbtn){turn();}
+
+		if(togetherbtn){together();}
+
+		execute();
+	}
 }
