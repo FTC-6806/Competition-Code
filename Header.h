ba6bc6222b29bc4cbd741c9acf34a35d80798902
Liam Marshall <archimedespi3141@gmail.com>
2015-01-19 14:00:58 -0600

    Pasted in old driving code. Won't compile

--- Header.h
+++ Header.h
@@ -26,3 +26,71 @@ int deadband = 5;
 float lowpower = 25;
 float normpower = 50;
 float highpower = 100;
+
+
+///////////////////////////////////////////////////////////////////
+//
+//                 Driving functions
+//
+//////////////////////////////////////////////////////////////////
+
+#define PI 3.14159265358979323846
+
+void turn(float t)
+{
+		t = (t / 360) * full_turn;
+
+		nMotorEncoder[Drive_R] = 0;
+		nMotorEncoder[Drive_L] = 0;
+
+ 		if (t>0)
+ 		{
+ 		nMotorEncoderTarget[Drive_R] = -1 *t;
+ 		nMotorEncoderTarget[Drive_L] = t;
+
+		motor[Drive_R] = -1 * drive_power;
+		motor[Drive_L] = drive_power;
+		}
+
+		else
+		{
+			nMotorEncoderTarget[Drive_R] = t;
+ 			nMotorEncoderTarget[Drive_L] = -1*t;
+
+			motor[Drive_R] = drive_power;
+			motor[Drive_L] = -1 * drive_power;
+		}
+
+		while((nMotorRunState[Drive_L] != runStateIdle) && (nMotorRunState[Drive_R] != runStateIdle)){wait1Msec(1);}
+
+		motor[Drive_R] = 0;
+		motor[Drive_L] = 0;
+
+		nMotorEncoder[Drive_R] = 0;
+		nMotorEncoder[Drive_L] = 0;
+		}
+
+void drive(float distance, bool dis = false)
+{
+	if (dis)
+	{
+	distance = distance / (4 * PI) * 1440;
+	}
+
+	nMotorEncoder[Drive_R] = 0;
+	nMotorEncoder[Drive_L] = 0;
+
+	nMotorEncoderTarget[Drive_R] = distance;
+ 	nMotorEncoderTarget[Drive_L] = distance;
+
+	motor[Drive_R] = drive_power;
+	motor[Drive_L] = drive_power;
+
+	while((nMotorRunState[Drive_L] != runStateIdle) && (nMotorRunState[Drive_R] != runStateIdle)){wait1Msec(1);}
+
+	motor[Drive_R] = 0;
+	motor[Drive_L] = 0;
+
+	nMotorEncoder[Drive_R] = 0;
+	nMotorEncoder[Drive_L] = 0;
+}
