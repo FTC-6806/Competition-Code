6a3a07b3b5957d4bf91b6e3be549bd97a878b824
Liam Marshall (via FTC 6806 laptop) <archimedespi3141@gmail.com>
2015-01-24 14:52:32 -0600

    Revert "Revert "Fixed joystick to wrong motor references""
    This reverts commit 8f16e159442755c6aad4ddb4f51687474ec17963.

--- Autonomous.c
+++ Autonomous.c
@@ -1,6 +1,6 @@
 #pragma config(Hubs,  S1, HTMotor, HTServo, none,  none)
-#pragma config(Motor,  mtr_S1_C1_1,     Drive_R,       tmotorTetrix, PIDControl, reversed, driveRight, encoder)
-#pragma config(Motor,  mtr_S1_C1_2,     Drive_L,       tmotorTetrix, PIDControl, driveLeft, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     Drive_R,       tmotorTetrix, PIDControl, reversed, driveRight, encoder)
+#pragma config(Motor,  mtr_S1_C1_1,     Drive_L,       tmotorTetrix, PIDControl, driveLeft, encoder)
 #pragma config(Servo,  srvo_S1_C2_1,		GoalGrabber1, tServoStandard)
 
 
