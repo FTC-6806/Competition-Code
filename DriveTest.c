6f67cc38c8db59baf81c5cb899b3d21347ac28f6
Liam Marshall (via FTC 6806 laptop) <archimedespi3141@gmail.com>
2015-01-24 17:41:51 -0600

    drive test

---
+++ DriveTest.c
@@ -0,0 +1,10 @@
+#pragma config(Hubs,  S1, HTMotor, HTServo, none,  none)
+#pragma config(Motor,  mtr_S1_C1_2,     Drive_R,       tmotorTetrix, PIDControl, driveRight, encoder)
+#pragma config(Motor,  mtr_S1_C1_1,     Drive_L,       tmotorTetrix, PIDControl, reversed, driveLeft, encoder)
+#pragma config(Servo,  srvo_S1_C2_1,		GoalGrabber1, tServoStandard)
+
+#include "Header.h"
+
+task main() {
+	drive_r(10, 100);
+}
