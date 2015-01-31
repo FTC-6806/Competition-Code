1a6296bdf73e78a711beef88f9b9660f236b6d17
Liam Marshall <archimedespi3141@gmail.com>
2015-01-31 15:16:04 -0600

    Revert "disable"
    This reverts commit e54c63dd69703565d8db433949a29471c58ee168.

--- DragGoal.c
+++ DragGoal.c
@@ -2,7 +2,7 @@
 #pragma config(Motor,  mtr_S1_C1_2,     Drive_R,       tmotorTetrix, PIDControl, driveRight, encoder)
 #pragma config(Motor,  mtr_S1_C1_1,     Drive_L,       tmotorTetrix, PIDControl, reversed, driveLeft, encoder)
 #pragma config(Servo,  srvo_S1_C2_1,		GoalGrabber1, tServoStandard)
-//#pragma config(Sensor, S2, touch, sensorTouch)
+#pragma config(Sensor, S2, touch, sensorTouch)
 
 #include "Header.h"
 
