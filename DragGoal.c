47ecae1c5454a501d8d339b799ab80ed0a64cc32
Liam Marshall <archimedespi3141@gmail.com>
2015-01-31 15:09:39 -0600

    stufftesting

--- DragGoal.c
+++ DragGoal.c
@@ -6,52 +6,56 @@
 
 #include "Header.h"
 
+
 task main() {
-	//int delayms = 1000 * get_delay_prompt();
-	waitForStart();
-	//wait1Msec(delayms);
-	drive_distance(-6*12, 50); // Drive 1 foot at 1/2 power
-	//turn_degrees(-5,35);
-
-	nMotorEncoder[Drive_R] = 0;
-	nMotorEncoder[Drive_L] = 0;
-
-
-	servoposition = 120;
-
-	servo[GoalGrabber1] = map(servoposition, 0, 360, 0, 255);
-
- nMotorEncoder[Drive_L] = 0;//reset the value of encoder B to zero
- nMotorEncoder[Drive_R] = 0;//reset the value of encoder C to zero
-
-
- while(SensorValue(touch) == 0)
-	{
-	motor[Drive_R] = -50;
-	motor[Drive_L] = -50;
-	}
-	motor[Drive_R] = 0;
-	motor[Drive_L] = 0;
-
-	servoposition = 10;
-	servo[GoalGrabber1] = map(servoposition, 0, 360, 0, 255);
-	delay(1000);
-	sleep(1000);
-	while(nMotorEncoder[Drive_L] < 0 && nMotorEncoder[Drive_R] < 0)
-	{
-	motor[Drive_R] = 50;
-	motor[Drive_L] = 50;
-	}
-	turn_degrees(30, 50);
-	drive_distance(6,50);
-	turn_degrees(30, 50);
-	drive_distance(6,50);
-	turn_degrees(30, 50);
-	drive_distance(2*12, 50);
-	turn_degrees(-90, 50);
-	drive_distance(4*12, 50);
-	turn_degrees(90,50);
-	drive_distance(3*12,50);
 	initSensor(&gyroSensor, S2);
 	startTask(getHeading);
+	//waitForStart();
+	wait1Msec(5000);
+	drive_rotations(0.5, 39);
+	////int delayms = 1000 * get_delay_prompt();
+	//waitForStart();
+	////wait1Msec(delayms);
+	//drive_distance(-6*12, 50); // Drive 1 foot at 1/2 power
+	////turn_degrees(-5,35);
+
+	//nMotorEncoder[Drive_R] = 0;
+	//nMotorEncoder[Drive_L] = 0;
+
+
+	//servoposition = 120;
+
+	//servo[GoalGrabber1] = map(servoposition, 0, 360, 0, 255);
+
+	//nMotorEncoder[Drive_L] = 0;//reset the value of encoder B to zero
+	//nMotorEncoder[Drive_R] = 0;//reset the value of encoder C to zero
+
+
+	//while(SensorValue(touch) == 0)
+	//{
+	//motor[Drive_R] = -50;
+	//motor[Drive_L] = -50;
+	//}
+	//motor[Drive_R] = 0;
+	//motor[Drive_L] = 0;
+
+	//servoposition = 10;
+	//servo[GoalGrabber1] = map(servoposition, 0, 360, 0, 255);
+	//delay(1000);
+	//sleep(1000);
+	//while(nMotorEncoder[Drive_L] < 0 && nMotorEncoder[Drive_R] < 0)
+	//{
+	//motor[Drive_R] = 50;
+	//motor[Drive_L] = 50;
+	//}
+	//turn_degrees(30, 50);
+	//drive_distance(6,50);
+	//turn_degrees(30, 50);
+	//drive_distance(6,50);
+	//turn_degrees(30, 50);
+	//drive_distance(2*12, 50);
+	//turn_degrees(-90, 50);
+	//drive_distance(4*12, 50);
+	//turn_degrees(90,50);
+	//drive_distance(3*12,50);
 }
