ea064d7e0bfe5d1979b19a834e7000921b207b0f
Liam Marshall <archimedespi3141@gmail.com>
2015-02-06 18:40:30 -0600

    New pragmas on autonomous

--- KnockOutPost.c
+++ KnockOutPost.c
@@ -1,7 +1,8 @@
-#pragma config(Hubs,  S1, HTMotor, HTServo, none,  none)
+#pragma config(Hubs,  S1, HTMotor, HTMotor, none,  none)
 #pragma config(Motor,  mtr_S1_C1_2,     Drive_R,       tmotorTetrix, PIDControl, driveRight, encoder)
 #pragma config(Motor,  mtr_S1_C1_1,     Drive_L,       tmotorTetrix, PIDControl, reversed, driveLeft, encoder)
-#pragma config(Servo,  srvo_S1_C2_1,		GoalGrabber1, tServoStandard)
+#pragma config(Motor, mtr_S1_C2_1,			Back_Arm,      tmotorTetrix, PIDControl, encoder);
+#pragma config(Motor, mtr_S1_C2_2,			Front_Arm,      tmotorTetrix, PIDControl, encoder);
 
 #include "Header.h"
 
