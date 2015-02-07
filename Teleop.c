221019762e5071b4d750c947aa3343a59fe32d28
Vincent Bensch <vbensch@gmail.com>
2015-02-07 11:42:14 -0600

    adieu bad P control.

--- Teleop.c
+++ Teleop.c
@@ -5,28 +5,6 @@
 
 #include "Header.h"
 
-int bTarget = 40;
-//int fTarget = 40;
-
-void zeroArms() {
-	nMotorEncoder[Back_Arm] = 0;
-	//nMotorEncoder[Front_Arm] = 0;
-	int lastback = 100;
-	//int lastfront = 100;
-	int a = 0;
-	//int b = 0;
-	motor[Back_Arm] = -10;
-	//motor[Front_Arm] = -10;
-	while(a != 1 /*&& b != 1*/)
-	{
-		lastback = nMotorEncoder(Back_Arm);
-		//lastfront = nMotorEncoder(Front_Arm);
-		wait1Msec(100);
-		if(nMotorEncoder[Back_Arm] == lastback){motor[Back_Arm] = 0; a = 1;}
-	}
-	nMotorEncoder[Back_Arm] = 0;
-}
-
 task Driver()
 {
 	while(true)
@@ -72,52 +50,29 @@ task DriveReverse()
 
 task Gunner() {
 	while (true) {
-		wait1Msec(10);
-		if ((joystick.joy2_y1 < -30)) {
-			bTarget -= 1;
-		} else if ((joystick.joy2_y1 > 30)) {
-			bTarget += 1;
-		}
-		if (resettozero) {
-			zeroArms();
-		}
-	}
-}
-
-task Arms() {
-	const int Kp = 1;
-	int bError = bTarget - nMotorEncoder[Back_Arm];
-	while (true) {
-		bError = bTarget - nMotorEncoder[Back_Arm];
-		int bPower = (int)(Kp*bError);
-		if (bPower > 100) { bPower = 100; } else if (bPower < -100) { bPower = -100; }
-		motor[Back_Arm] = bPower;
+		wait1Msec(50);
+		motor[Back_Arm] = map(joystick.joy2_y1, -128, 128, -15, 15);
 	}
 }
 
 task main()
 {
 	waitForStart();
-	zeroArms();
 	startTask(Driver);
 	startTask(DriveReverse);
-	startTask(Arms);
 	startTask(Gunner);
 
 	while(true)
 	{
-		displayTextLine(0, "%f", bTarget);
 		if(driverstop || gunnerstop){
 			stopTask(Driver);
 			stopTask(Gunner);
-			stopTask(Arms);
 			motor[Drive_R] = 0;
 			motor[Drive_L] = 0;
 			motor[Back_Arm] = 0;
 			while(driverstop || gunnerstop){wait1Msec(1);}
 			startTask(Driver);
 			startTask(Gunner);
-			startTask(Arms);
 		}
 	}
 }
